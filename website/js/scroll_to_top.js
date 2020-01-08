0d1fda83b70fd62c433d8c422e08d03d65b02d6e
Tom Elam <tomelam@gmail.com>
2020-01-08 18:22:37 +0530

    Fixed the reference to the link we are trying to jump to after page load.

--- website/js/scroll_to_top.js
+++ website/js/scroll_to_top.js
@@ -1,2 +1,3 @@
 //$("a").get(4).click();  // Simulate a click on the second link in the page.
-$("#sequentially-programmed-web-app-demo").click();
+console.log("Number of links in the page: " + $("a").length);
+$("#sequentially-programmed-web-app-demo").get(0).click();
