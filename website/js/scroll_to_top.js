70299eb8c131d24e1745d0acdd3628606fcfa119
Tom Elam <tomelam@gmail.com>
2020-01-08 18:30:16 +0530

    Created a function to scroll to top at document ready.

--- website/js/scroll_to_top.js
+++ website/js/scroll_to_top.js
@@ -1,3 +1,6 @@
 //$("a").get(4).click();  // Simulate a click on the second link in the page.
 console.log("Number of links in the page: " + $("a").length);
-$("#sequentially-programmed-web-app-demo").get(0).click();
+function scroll_to_top() {
+    console.log("Number of links in the page: " + $("a").length);
+    $("#sequentially-programmed-web-app-demo").get(0).click();
+}
